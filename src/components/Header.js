e133884de57ddb7f7b978a4a2e9863bb5017296b
PBoyal20 <129793660+PBoyal20@users.noreply.github.com>
2024-01-28 10:56:25 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <img className="header--img" src="/Troll-Face.png"/>
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Project - Prabhi Boyal</h4>
+        </header>
+    );
+    }
